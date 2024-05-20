60a1ef82159078dd56d1a07cf6e1ed7966da44e9
khalifa5000 <m.khalifa5000@gmail.com>
2024-05-20 13:10:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+  <h1>Hello to my app</h1>
+  <h2>Welcome</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
