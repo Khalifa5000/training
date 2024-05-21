8adca36b77013e88c382348eb89ebd168b06bf5b
khalifa5000 <m.khalifa5000@gmail.com>
2024-05-21 23:21:39 +0300

    Add components

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+        <footer>
+          Designed and developed by Mohamed Khalifa.com
+          <span>🧡</span>
+        </footer>
+    </div>
+  );
+}
+
+export default Footer;
