41be75c763c51f67c6aa5dbb1a895921f383b718
khalifa5000 <m.khalifa5000@gmail.com>
2024-05-21 21:28:19 +0300

    Edit react router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./pages/home";
+import Html from './pages/html';
+import Css from './pages/css';
+import Javascript from './pages/javascript';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h2>Page not found</h2>
+  },
+  {
+    path: "/html",
+    element: <Html/>,
+
+  },
+  {
+    path: "/css",
+    element: <Css/>,
+
+  },
+  {
+    path: "/javascript",
+    element: <Javascript/>,
+
+  },
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
